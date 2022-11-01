feff62fb0d7aa375841c0019d24b7286bb9dd628
Akhila Sulgante <asulgante39@gmail.com>
2022-10-31 19:50:34 -0700

    env file update

--- db/MyMongoDB.js
+++ db/MyMongoDB.js
@@ -4,7 +4,8 @@ require("dotenv").config();
 
 function MyMongoDB() {
   const myDB = {};
-  const url = "mongodb+srv://admin-ted:Test123@cluster0.dz0wqq8.mongodb.net/";
+  const url = "mongodb://localhost:27017" || process.env.DB_URL;
+
   const DB_NAME = "baby-stuff-sharing-db";
 
   myDB.read = async (collectionName, query) => {
