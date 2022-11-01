9f8ba9fff69c11fe9623af17db40f144a974db1f
Zhiyi Jin <victoryjzy@gmail.com>
2022-11-01 12:12:39 -0700

    fix bug of mongodb url

--- db/MyMongoDB.js
+++ db/MyMongoDB.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 function MyMongoDB() {
   const myDB = {};
-  const url = "process.env.DB_URL || mongodb://localhost:27017";
+  const url = process.env.DB_URL || "mongodb://localhost:27017";
   const DB_NAME = "baby-stuff-sharing-db";
 
   myDB.read = async (collectionName, query) => {
