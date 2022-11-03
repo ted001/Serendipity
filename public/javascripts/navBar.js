3e38fae32d4929723bd273fb2e7a03ca846f36a5
m-siegel <87347495+m-siegel@users.noreply.github.com>
2022-11-03 11:35:22 -0700

    Code review comments
    I like that you created this one file to control the navbars on every page. The way you changed the contents of elements based on the page or the user's logged in status was cool. I left a couple notes on things that I would change, too. Good work!

--- public/javascripts/navBar.js
+++ public/javascripts/navBar.js
@@ -1,17 +1,28 @@
 //Akhila
+
+// Puting the JS for the navbars that are shared across pages was a great idea!
+// It's a good example of how modularization allows you to not repeat code that will be reused,
+// and to only have to make changes in one place when you want to change other things. Nicely done!
 async function isUserLogged() {
   try {
     const name = sessionStorage.getItem("email");
     if (name != "null" && name != null) {
       let updateSignUp = document.getElementById("signup");
       console.log("element signout", updateSignUp);
+      // This is also pretty neat!!
       updateSignUp.innerHTML = `Sign out`;
       updateSignUp.href = "../index.html";
       let updateUserName = document.getElementById("signin");
+      // I like that you put the person's name in the navbar -- it gives a personal feel.
+      // Without changing this element's href, it still directs to the login page, which was a little confusing.
+      // I'd recommend changing what it links to as well as what it says.
+      updateUserName.setAttribute("href", "#");
       updateUserName.innerHTML = `Hi ${name}`;
       // Adding the create button for posts
       let createPosts = document.getElementById("createPosts");
-      let logout = document.getElementById("logout");
+      // I didn't see this used elsewhere in this file and I didn't see a logout id in the html
+//       let logout = document.getElementById("logout");
+      // This is cool -- checking where you are to determine what to display
       if (window.location.pathname === "/postList.html") {
         let btn = document.createElement("button");
         btn.style.backgroundColor = "transparent";
